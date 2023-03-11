0e71db150496877cea3df0abde333748d7c96407
Charls Brent Cayabyab <117609343+CharlsCayabyab27@users.noreply.github.com>
2023-03-11 15:33:23 +0800

    add filter
    add filter

---
+++ filter.php
@@ -0,0 +1,7 @@
+<form action="filter.php" method="get">
+    <label for="start_date">Start Date:</label>
+    <input type="date" name="start_date" id="start_date">
+    <label for="end_date">End Date:</label>
+    <input type="date" name="end_date" id="end_date">
+    <input type="submit" value="Filter">
+</form>
